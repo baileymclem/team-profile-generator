a72466b04e7b3fad45c2c2264af6254565447a50
Bailey Clemons <baileythevampireslayer@gmail.com>
2020-12-23 17:35:57 -0800

    "finished modules and tests"

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,35 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    // Just like constructor functions, classes can accept arguments
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+
+    // printInfo() {
+    //   console.log(`Area: ${this.area}`);
+    //   console.log(`Perimeter: ${this.perimeter}`);
+    // }
+
+    getName() {
+        console.log("name", this.name)
+        return this.name
+    }
+
+    getId() {
+        return this.id
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return "Employee"
+    }
+  }
+
+  module.exports = Employee
+
